2281736b675e37886964990c4e3b2b129b881b4c
Chris Anderson <chrisanderson@developers.institute.co.nz>
2023-12-09 15:34:40 +1300

    initialised tailwind and confirmed working

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Robot from "./assets/robot.jpg";
+import "./index.css";
 
 const App = () => {
   return (
-    <div data-testid="idForTesting" className="main">
-      <img src={Robot} alt={"chatbot"} />
+    <div
+      data-testid="idForTesting"
+      className="main flex justify-center items-center h-screen"
+    >
+      <img src={Robot} alt={"chatbot"} className="w-1/2 h-auto" />
     </div>
   );
 };
