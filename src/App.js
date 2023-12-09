83514d17271925e0e666760f7141ccca069a28c3
Chris Anderson <chrisanderson@developers.institute.co.nz>
2023-12-09 15:16:34 +1300

    added robot image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import Robot from "./assets/robot.jpg";
+
 const App = () => {
-  return <div data-testid="idForTesting">hello</div>;
+  return (
+    <div data-testid="idForTesting" className="main">
+      <img src={Robot} alt={"chatbot"} />
+    </div>
+  );
 };
 
 export default App;
