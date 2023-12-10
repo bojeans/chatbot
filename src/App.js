56a52a3ba94b1354becfe6d26037658488cbe16c
Chris Anderson <chrisanderson@developers.institute.co.nz>
2023-12-10 17:48:59 +1300

    adding state for title to change once robot clicked

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Robot from "./assets/robot.jpg";
 const App = () => {
   const [showSpeechBubble, setShowSpeechBubble] = useState(false);
   const [initialMessage, setInitialMessage] = useState("");
+  const [title, setTitle] = useState(
+    "Click on the floating robot to start chatting!"
+  );
 
   const handleStartChat = () => {
     setShowSpeechBubble(true);
+    setTitle(
+      "If you get stuck, just type 'help' and I'll assist you. If you want to start over, just type 'reset'."
+    );
     setInitialMessage(
       ` Hi there! I'm your "friendly" chatbot. What's your name? `
     );
@@ -15,7 +21,7 @@ const App = () => {
   return (
     <>
       <div>
-        <p>Click on the floating robot to start chatting!</p>
+        <p>{title}</p>
       </div>
       <div
         data-testid="idForTesting"
